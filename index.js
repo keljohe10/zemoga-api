1d178f4081a477ae1388a7b81d78a090d109604f
kelvin jose hernandez cabrera <kelhzkb@hotmail.com>
2021-11-19 15:56:14 -0500

    add env

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ require("dotenv").config();
 const app = require("./src/app");
 const {
   NODE_ENV = "development",
-  PORT = 3005,
+  PORT = 3001,
   USER_MONGO,
   PASSWORD_MONGO,
+  DATABASE,
 } = process.env;
 const mongoose = require("mongoose");
 
 async function StartServer() {
   mongoose.connection.openUri(
-    `mongodb+srv://${USER_MONGO}:${PASSWORD_MONGO}@cluster0.patpy.mongodb.net/nodeplatzi?retryWrites=true&w=majority`,
+    `mongodb+srv://${USER_MONGO}:${PASSWORD_MONGO}@${DATABASE}?retryWrites=true&w=majority`,
     (err, res) => {
       if (err) throw err;
       console.log("DB \x1b[34m%s\x1b[0m", "Online!!");
